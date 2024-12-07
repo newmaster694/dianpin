d7fbd56246678d1e8b7e8d101b7700d07d00cad0
newmaster <2338946313@qq.com>
2024-12-07 23:55:27 +0800

    version 0.3.3 一人一单加强版->分布式锁->调用lua脚本解决释放锁的原子性问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,7 +1,10 @@
 package com.hmdp.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -17,10 +20,23 @@ public class SimpleRedisLock implements ILock {
 	
 	private static final String KEY_PREFIX = "lock:";
 	private static final String ID_PREFIX = UUID.randomUUID() + "-";//防止误删锁的线程标识
+	private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
 	
+	static {
+		UNLOCK_SCRIPT = new DefaultRedisScript<>();
+		UNLOCK_SCRIPT.setLocation(new ClassPathResource("/script/unlock.lua"));
+		UNLOCK_SCRIPT.setResultType(Long.class);
+	}
+	
+	/**
+	 * 获取锁
+	 *
+	 * @param timeoutSec 锁持有的超过时间,过期后自动释放
+	 * @return
+	 */
 	@Override
 	public boolean tryLock(Long timeoutSec) {
-		//获取线程提示
+		//获取线程标识
 		String currentThreadId = ID_PREFIX + Thread.currentThread().getId();
 		
 		//获取锁(键=>当前业务名;值=>当前线程id)
@@ -30,18 +46,16 @@ public class SimpleRedisLock implements ILock {
 		return Boolean.TRUE.equals(flag);
 	}
 	
+	/**
+	 * 释放锁
+	 */
 	@Override
 	public void unlock() {
-		//获取线程标识
-		String currentThreadId = ID_PREFIX + Thread.currentThread().getId();
-		
-		//获取锁标识
-		String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-		
-		//比较标识是否一致
-		if (currentThreadId.equals(id)) {
-			//释放锁
-			stringRedisTemplate.delete(KEY_PREFIX + name);
-		}
+		//调用lua脚本解决释放锁的原子性问题
+		stringRedisTemplate.execute(
+				UNLOCK_SCRIPT,
+				Collections.singletonList(KEY_PREFIX + name),
+				ID_PREFIX + Thread.currentThread().getId()
+		);
 	}
 }
--- src/main/resources/script/unlock.lua
+++ src/main/resources/script/unlock.lua
@@ -1,10 +1,10 @@
--- 锁的key
+-- 锁的key=>KEY_PREFIX + name
 local key = KEYS[1]
 
--- 当前线程标识
+-- 当前线程标识=>currentThreadId
 local threadId = ARGV[1]
 
--- 获取锁中的线程标识
+-- 获取锁中的线程标识=>currentThreadId
 local id = redis.call('get', key)
 
 -- 比较线程中的标识与锁中的标识是否一致
